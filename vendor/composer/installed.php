062ddea4c67cf2c6b0a506b12ecb794e078347e0
CP Marketing Digital e Podcast <226177847+sites-cpmarketing@users.noreply.github.com>
2025-11-25 12:35:34 -0300

    V

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'natucart/backend',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b3c545d274d95c719821d82f523e2f79cbff2360',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mercadopago/dx-php' => array(
+            'pretty_version' => '3.7.1',
+            'version' => '3.7.1.0',
+            'reference' => 'ca1c9b495b6b5ba75bed92652110b1ad9ea834c6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mercadopago/dx-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'natucart/backend' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b3c545d274d95c719821d82f523e2f79cbff2360',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
